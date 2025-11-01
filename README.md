883bfa9b6973b7ef11c3a3b8bfa6aefc9cee3a62
Glitch (hello-webpage) <none>
2025-11-01 15:03:40 -0400

    feat: add slider to update dvd's velocity
    also fixed canvas to be a true background fixed to the viewport w/ 'position: fixed' and 'z-index: -1' so that slider can go on top.

--- README.md
+++
@@ -1,9 +0,0 @@
-CSSI Day 2
-=================
-
-
-
-- README.md - This file!
-- index.html - Html page base structure, we include dependencies like JS files here.
-- sketch.js - Your p5js code goes here.
-- style.css - CSS styles for the page.
--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 /* global createCanvas background image loadImage windowWidth windowHeight width height*/
 
 let dvdImage, imageX, xVelocity, imageY, yVelocity;
-const globalVelocity = 4;
-imageWidth = 200;
-imageHeight = 150;
+let globalVelocity = 2;
+
+const imageWidth = 200;
+const imageHeight = 150;
+
+let velocitySlider;
+let speedDisplay;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -16,6 +20,29 @@ function setup() {
   dvdImage = loadImage(
     "https://cdn.glitch.com/2c8c05ca-f0b5-426e-9d1a-07a2d87bd9f6%2Fdvd-video.jpeg?v=1594094901767"
   );
+  
+  setupSlider();
+}
+
+function setupSlider() {
+  velocitySlider = document.getElementById('velocitySlider');
+  speedDisplay = document.getElementById('speedDisplay');
+  
+  globalVelocity = parseFloat(velocitySlider.value);
+  speedDisplay.innerText = globalVelocity;
+  xVelocity = globalVelocity;
+  yVelocity = globalVelocity;
+  
+  // Update velocity
+  velocitySlider.addEventListener('input', (e) => {
+    const newSpeed = parseFloat(e.target.value);
+    globalVelocity = newSpeed;
+    speedDisplay.innerText = newSpeed;
+
+    // Math.sign() returns 1 or -1 based on the number sign
+    xVelocity = Math.sign(xVelocity) * newSpeed;
+    yVelocity = Math.sign(yVelocity) * newSpeed;
+  });
 }
 
 function draw() {
